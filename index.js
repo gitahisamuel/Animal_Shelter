e95547a43398ecefd9bcf309932a5a3a5240f68f
gitahisamuel <murimisamuel05@gmail.com>
2025-09-24 23:49:29 +0300

    updated javascript

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const Url = "";
+const cardContainer = document.querySelector("#card-container");
+// const card = document.querySelector("#card");
+// const image = document.querySelector("#image");
+// const description= document.querySelector("#description");
+// const adoptBtn = document.querySelector("#btn-adopt");
+// const donateBtn = document.querySelector("#btn-donate");
+const api_url = "http://localhost:3000/animals";
+
+  fetch(api_url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+
+        },
+        body: JSON.stringify()
+
+    }).then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+
+data.forEach(animal => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+    <h2>${animal.name}</h2>
+    <p>${animal.type}</p>
+    `;
+    cardContainer.appendChild(card)
+
+
+})
+
