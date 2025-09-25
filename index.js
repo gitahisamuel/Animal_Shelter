2806da07840f4e62fe68db496fa1c743f2fc89f2
gitahisamuel <murimisamuel05@gmail.com>
2025-09-25 11:29:52 +0300

    updated javascript

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-const cardContainer = document.querySelector("#card-container");
-// const card = document.querySelector("#card");
-// const image = document.querySelector("#image");
-// const description= document.querySelector("#description");
-// const adoptBtn = document.querySelector("#btn-adopt");
-// const donateBtn = document.querySelector("#btn-donate");
-const api_url = "http://localhost:3000/animals";
+document.addEventListener("DOMContentLoaded", () => {
+    const cardContainer = document.querySelector("#card-container");
+    const card = document.querySelector("#card");
+    // const image = document.querySelector("#image");
+    // const description= document.querySelector("#description");
+    // const adoptBtn = document.querySelector("#btn-adopt");
+    // const donateBtn = document.querySelector("#btn-donate");
+    const api_url = "http://localhost:3000/animals";
 
-  fetch(api_url, {
+    fetch(api_url, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
             Accept: "application/json",
 
         },
-        body: JSON.stringify()
+
 
     }).then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
+        .then(data => {
+            // console.log(data);
+        });
+    for (const animal of data) {
+        const card = document.createElement("div");
+        card.classList.add("card");
 
-data.forEach(animal => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    card.innerHTML = `
-    <h2>${animal.name}</h2>
-    <p>${animal.type}</p>
-    `;
-    cardContainer.appendChild(card)
+        card.innerHTML = `
+        <h2>${animal.name}</h2>
+        <p>${animal.type}</p>
 
 
-})
+      `;
+
+        cardContainer.appendChild(card);
+    }
+});
+
+
+
+
+
 
